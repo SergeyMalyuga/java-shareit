d808d9894a16ce05d146684c37ea97f31082a568
Sergey Malyuga <sergey@malyuga.ru>
2023-10-25 10:20:58 +0300

    refactor: создать enum BookingStatus

---
+++ src/main/java/ru/practicum/shareit/exception/NoDataFoundException.java
@@ -0,0 +1,7 @@
+package shareit.exception;
+
+public class NoDataFoundException extends RuntimeException {
+    public NoDataFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -1,4 +1,4 @@
-package ru.practicum.shareit.request;
+package shareit.request;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
