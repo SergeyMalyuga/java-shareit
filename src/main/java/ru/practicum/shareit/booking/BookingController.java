aa9e0bcc722bfaf71735736a6b2dc1be7668a219
Andersen <sergey@malyuga.ru>
2023-11-14 14:55:00 +0300

    refactor: создать shema.sql, exception UnavailableStateException.java, UnavailableItemException.java, PostWithoutBookingException.java, BookingServiceImpl, BookingController

--- src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -1,12 +1,49 @@
 package ru.practicum.shareit.booking;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.booking.service.BookingService;
+import ru.practicum.shareit.user.service.UserService;
 
-/**
- * TODO Sprint add-bookings.
- */
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "/bookings")
 public class BookingController {
+
+    @Autowired
+    private BookingService bookingService;
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public Booking addBooking(@RequestBody Booking booking, @RequestHeader("X-Sharer-User-Id") int userId) {
+        userService.getUserById(userId);
+        return bookingService.addBooking(booking, userId);
+    }
+
+    @PatchMapping("/{bookingId}")
+    public Booking confirmationOrRejectionBooking(@PathVariable int bookingId,
+                                                  @RequestHeader("X-Sharer-User-Id") int userId,
+                                                  @RequestParam(name = "approved") String bookingStatus) {
+        return bookingService.confirmationOrRejectionBooking(bookingId, userId, bookingStatus);
+    }
+
+    @GetMapping("/{bookingId}")
+    public Booking getBookingById(@RequestHeader("X-Sharer-User-Id") int userId, @PathVariable int bookingId) {
+        return bookingService.getBookingById(bookingId, userId);
+    }
+
+    @GetMapping()
+    public List<Booking> getAllBookingCurrentUser(@RequestHeader("X-Sharer-User-Id") int userId,
+                                                  @RequestParam(name = "state", defaultValue = "ALL") String state) {
+        return bookingService.getAllBookingCurrentUser(userId, state);
+    }
+
+    @GetMapping("/owner")
+    public List<Booking> getAllBookingCurrentOwner(@RequestHeader("X-Sharer-User-Id") int userId,
+                                                   @RequestParam(name = "state", defaultValue = "ALL") String state) {
+        return bookingService.getAllBookingCurrentOwner(userId, state);
+    }
+
 }
