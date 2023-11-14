66ac72640ca0eb6408766d72f2ca6a1ccf70ad98
Andersen <sergey@malyuga.ru>
2023-11-14 14:55:00 +0300

    refactor: создать shema.sql, exception UnavailableStateException.java, UnavailableItemException.java, PostWithoutBookingException.java, BookingServiceImpl, BookingController

---
+++ src/main/java/ru/practicum/shareit/booking/service/BookingService.java
@@ -0,0 +1,17 @@
+package ru.practicum.shareit.booking.service;
+
+import ru.practicum.shareit.booking.Booking;
+
+import java.util.List;
+
+public interface BookingService {
+    Booking addBooking(Booking booking, int bookerId);
+
+    Booking getBookingById(int bookingId, int userId);
+
+    List<Booking> getAllBookingCurrentUser(int userId, String state);
+
+    List<Booking> getAllBookingCurrentOwner(int userId, String state);
+
+    Booking сonfirmationOrRejectionBooking(int bookingId, int userId, String bookingStatus);
+}
