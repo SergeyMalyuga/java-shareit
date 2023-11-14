a34fd6b198df3fae3d9b3b16032401457936f111
Andersen <sergey@malyuga.ru>
2023-11-14 14:55:00 +0300

    refactor: создать shema.sql, exception UnavailableStateException.java, UnavailableItemException.java, PostWithoutBookingException.java, BookingServiceImpl, BookingController

--- src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -20,4 +20,21 @@ public class ErrorHandler {
         return new ErrorResponse(e.getMessage(), "Данный email уже существует.");
     }
 
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse unavailableItem(UnavailableItemException e) {
+        return new ErrorResponse(e.getMessage(), "Данная вещь не доступна для бронирования.");
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse unavailableState(UnavailableStateException e) {
+        return new ErrorResponse(e.getMessage(), "Не верный статус.");
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse postWithoutBooking(PostWithoutBookingException e) {
+        return new ErrorResponse(e.getMessage(), "Пользователь не может оставить комментарий к данной вещи.");
+    }
 }
