572d737260a58b26a62fa4fe3f98e3adca0ac94d
Andersen <sergey@malyuga.ru>
2023-11-14 14:55:00 +0300

    refactor: создать shema.sql, exception UnavailableStateException.java, UnavailableItemException.java, PostWithoutBookingException.java, BookingServiceImpl, BookingController

--- src/main/java/ru/practicum/shareit/constants/MyConstants.java
+++ src/main/java/ru/practicum/shareit/constants/MyConstants.java
@@ -1,29 +1,29 @@
 package ru.practicum.shareit.constants;
 
 public class MyConstants {
-    public static final String ADD_USER_POINTCUT = "execution(public ru.practicum.shareit.user.dto.UserDto ru.practicum.shareit.user.dao.UserDaoImpl.addUser" +
-            "(ru.practicum.shareit.user.User))";
-    public static final String REMOVE_USER_POINTCUT = "execution(public String ru.practicum.shareit.user.dao.UserDaoImpl.removeUserById" +
-            "(*))";
+    public static final String ADD_USER_POINTCUT = "execution(public ru.practicum.shareit.user.User " +
+            "ru.practicum.shareit.user.service.UserServiceImpl.addUser(ru.practicum.shareit.user.User))";
+    public static final String REMOVE_USER_POINTCUT = "execution(public void " +
+            "ru.practicum.shareit.user.service.UserServiceImpl.removeUserById(*))";
 
-    public static final String GET_ALL_USERS_POINTCUT = "execution(public java.util.List<ru.practicum.shareit.user.dto.UserDto> " +
-            "ru.practicum.shareit.user.dao.UserDaoImpl.getAllUsers())";
+    public static final String GET_ALL_USERS_POINTCUT = "execution(public java.util.List<ru.practicum.shareit.user.User> " +
+            "ru.practicum.shareit.user.service.UserServiceImpl.getAllUsers())";
 
     public static final String UPDATE_USER_POINTCUT = "execution(public ru.practicum.shareit.user.dto.UserDto " +
-            "ru.practicum.shareit.user.dao.UserDaoImpl.UpdateUser(..))";
+            "ru.practicum.shareit.user.service.UserServiceImpl.UpdateUser(..))";
 
     public static final String ADD_ITEM_POINTCUT = "execution(public ru.practicum.shareit.item.dto.ItemDto " +
-            "ru.practicum.shareit.item.dao.ItemDaoImpl.addItem(..))";
+            "ru.practicum.shareit.item.service.ItemServiceImpl.addItem(..))";
 
     public static final String UPDATE_ITEM_POINTCUT = "execution(public ru.practicum.shareit.item.dto.ItemDto " +
-            "ru.practicum.shareit.item.dao.ItemDaoImpl.updateItem(..))";
+            "ru.practicum.shareit.item.service.ItemServiceImpl.updateItem(..))";
 
     public static final String GET_ITEM_BY_ID_POINTCUT = "execution(public ru.practicum.shareit.item.dto.ItemDto " +
-            "ru.practicum.shareit.item.dao.ItemDaoImpl.getItemById(..))";
+            "ru.practicum.shareit.item.service.ItemServiceImpl.getItemDtoById(..))";
 
     public static final String GET_ALL_ITEM_POINTCUT = "execution(public java.util.List<ru.practicum.shareit.item.dto.ItemDto> " +
-            "ru.practicum.shareit.item.dao.ItemDaoImpl.getAllItemForOwner(..))";
+            "ru.practicum.shareit.item.service.ItemServiceImpl.getAllItemForOwner(..))";
 
     public static final String SEARCH_ITEM_POINTCUT = "execution(public java.util.List<ru.practicum.shareit.item.dto.ItemDto> " +
-            "ru.practicum.shareit.item.dao.ItemDaoImpl.searchItem(..))";
+            "ru.practicum.shareit.item.service.ItemServiceImpl.searchItem(..))";
 }
