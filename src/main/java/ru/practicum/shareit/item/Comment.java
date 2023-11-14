fa9ac1d7de79a3737edc574bf5f49dff976e7cf0
Andersen <sergey@malyuga.ru>
2023-11-14 14:55:00 +0300

    refactor: создать shema.sql, exception UnavailableStateException.java, UnavailableItemException.java, PostWithoutBookingException.java, BookingServiceImpl, BookingController

---
+++ src/main/java/ru/practicum/shareit/item/Comment.java
@@ -0,0 +1,31 @@
+package ru.practicum.shareit.item;
+
+import lombok.Data;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.User;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "comments")
+@Data
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "text")
+    private String text;
+    @ManyToOne
+    @JoinColumn(name = "item_id")
+    private Item item;
+    @ManyToOne
+    @JoinColumn(name = "author")
+    private User author;
+    @Column(name = "created")
+    private LocalDateTime created;
+    @Transient
+    String authorName;
+}
